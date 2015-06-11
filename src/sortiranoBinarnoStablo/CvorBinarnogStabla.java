d852920fa3a936c2c2cfa52d43839ca712e04de5
Tatjana <vasiljevictatjana@yahoo.com>
2015-06-11 23:49:09 +0200

    Napravljena klasa CvorBinarnogStabla

---
+++ src/sortiranoBinarnoStablo/CvorBinarnogStabla.java
@@ -0,0 +1,21 @@
+package sortiranoBinarnoStablo;
+
+public class CvorBinarnogStabla {
+	
+	public String vrednost;
+	public int kljuc;
+	
+	public String getVrednost() {
+		return vrednost;
+	}
+	public void setVrednost(String vrednost) {
+		this.vrednost = vrednost;
+	}
+	public int getKljuc() {
+		return kljuc;
+	}
+	public void setKljuc(int kljuc) {
+		this.kljuc = kljuc;
+	}
+
+}
